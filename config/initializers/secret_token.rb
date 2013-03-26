d3e6b73f3287ce8e9931f4cbc0fe4ba76fd647aa
Ubuntu 12.04 <neel@ubuntu.(none)>
2013-03-26 03:04:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omcrafts::Application.config.secret_token = '73f2d278b746c4dac6fbd54a3f4af7130fe573cba9c2e5b7937d02db170d5375a5501ab47d664816bda4d69664d045e9d319c5f25cb4baeff2210c033459cc59'
